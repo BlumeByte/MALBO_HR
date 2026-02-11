34f4e814d1d2ef31b4f5c7c71e8103d9b65dc725
Dzornoboa <cstmrsolution@gmail.com>
2026-02-11 14:01:08 +0000

    trial one of database connection

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,9 +1,9 @@
 class Env {
   static const supabaseUrl = String.fromEnvironment(
-    'https://mggoypriigytfqlftrlk.supabase.co',
+    'SUPABASE_URL',
   );
 
   static const supabaseAnonKey = String.fromEnvironment(
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nZ295cHJpaWd5dGZxbGZ0cmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MjI0OTIsImV4cCI6MjA4NjI5ODQ5Mn0.ea9dglvyg_llBmO9M4CxI11o1hCBGOAtK_wNTplxmfM',
+    'SUPABASE_ANON_KEY',
   );
 }
