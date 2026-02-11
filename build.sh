8d66fab5857ebc2d9c89ad146534877e71fd75de
Dzornoboa <cstmrsolution@gmail.com>
2026-02-11 14:30:35 +0000

    Final works for vercel
    previous ones didn't work

--- build.sh
+++ build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
 set -e
 
-echo "Cloning Flutter..."
+echo "Installing Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 
-echo "Adding Flutter to PATH..."
 export PATH="$PWD/flutter/bin:$PATH"
 
-flutter doctor
+flutter --version
 flutter pub get
 
 echo "Building Flutter Web..."
-flutter build web --release --web-renderer canvaskit \
+flutter build web --release \
   --dart-define=SUPABASE_URL=$SUPABASE_URL \
   --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
