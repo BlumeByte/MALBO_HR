ebe7452c8af584348bd7aac14d09feff1aa55f89
Dzornoboa <cstmrsolution@gmail.com>
2026-02-11 10:45:57 +0000

    Uploading to Vercel
    First trial

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,11 +1,9 @@
 class Env {
   static const supabaseUrl = String.fromEnvironment(
-    'SUPABASE_URL',
-    defaultValue: 'https://YOUR_PROJECT_ID.supabase.co',
+    'https://mggoypriigytfqlftrlk.supabase.co',
   );
 
   static const supabaseAnonKey = String.fromEnvironment(
-    'SUPABASE_ANON_KEY',
-    defaultValue: 'YOUR_PUBLIC_ANON_KEY',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nZ295cHJpaWd5dGZxbGZ0cmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MjI0OTIsImV4cCI6MjA4NjI5ODQ5Mn0.ea9dglvyg_llBmO9M4CxI11o1hCBGOAtK_wNTplxmfM',
   );
 }
