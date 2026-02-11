72ea42818b083cb40eb08c4f89d800d260f193ee
Dzornoboa <cstmrsolution@gmail.com>
2026-02-11 14:23:44 +0000

    Changes to build deploy on vercel first build

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+set -e
+
+echo "Cloning Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
+
+echo "Adding Flutter to PATH..."
 export PATH="$PWD/flutter/bin:$PATH"
 
-flutter --version
+flutter doctor
 flutter pub get
 
+echo "Building Flutter Web..."
 flutter build web --release --web-renderer canvaskit \
   --dart-define=SUPABASE_URL=$SUPABASE_URL \
   --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
