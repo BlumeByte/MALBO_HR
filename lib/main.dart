7f83e7881bd2f54ec4f0203fba17ad8a605b3ba1
Dzornoboa <cstmrsolution@gmail.com>
2026-02-12 17:22:50 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,5 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseService.init();
 
-  runApp(
-    const ProviderScope(
-      child: MalboHRApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MalboHRApp()));
 }
